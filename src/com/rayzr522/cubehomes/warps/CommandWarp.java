ecb1ed92841f31694b94227acc6b99e11497ef84
Rayzr522 <rayzr522@users.noreply.github.com>
2016-09-13 10:47:56 -0400

    Added world check

--- src/com/rayzr522/cubehomes/warps/CommandWarp.java
+++ src/com/rayzr522/cubehomes/warps/CommandWarp.java
@@ -37,7 +37,7 @@ public class CommandWarp implements CommandExecutor {
 
 		Warp warp = Warps.get(args[0]);
 
-		if (warp == null) {
+		if (warp == null || warp.getWorld() != p.getWorld()) {
 
 			Msg.send(sender, "unknown-warp", args[0]);
 			return true;
